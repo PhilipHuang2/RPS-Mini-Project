354ed71abe9fbb5c02f5ab2b8975d9ca9b4b490e
Omer <omerweiss@yahoo.com>
2022-09-28 15:40:21 -0700

    added evaluator function, playagain function, generatecomputerchoice function, and parseuserinput funtion

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 // 3. See who wins
 // 4. Tell user who wons
 // 5. add to counter of wins/losses and ties
-// 6. prompt user to replay game
+// 6. Display results
+// 7. prompt user to replay game
 
 // Variables
 // User Choice - 
@@ -18,11 +19,78 @@
 // Determining computer choice
 
 
+
+
+// initialize counters
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// expected range is 0,1,2
+
+console.log(Math.floor(Math.random() * 3));
+
+console.log(generateComputerChoice());
 // Ask the user at least once for their valid input
+
+
+// create a function that compares the values stored for user choice and computer choice
+
+function evaluator(user,computer){
+    if (user == computer){
+        console.log("It's a tie");
+        ties = ties + 1;
+    }
+    else if ((user == "R" && computer == "S" ) || (user == "S" && computer == "P" ) || (user == "P" && computer == "R" )){
+        console.log("User Wins");
+        wins = wins +1;
+    }else {
+        console.log("Computer Wins");
+        losses = losses + 1;
+    }
+}
+
+console.log(evaluator("R","S"));
+
 do {
     var userInput = prompt("Please enter R, P, or S to play Rock Paper Scissors.");
     userInput = parseUserInput(userInput);
 } while (!userInput)
+
+confirm("Wins: " + wins + "\n Loses: " + losses + "\nTies: " + ties)
+
+function playAgain (){
+    do {
+        var userInput = prompt("Please enter R, P, or S to play Rock Paper Scissors.");
+        userInput = parseUserInput(userInput);
+    } while (!userInput);
+    prompt("Wins: " + wins + "\n Loses: " + losses + "\nTies: " + ties);
+    if (confirm("Do you want to play again?")){
+     playAgain();
+   }
+    
+}
+
+// Store userInput into variable for counter
+
+var userInputCounter = userInput;
+
+//Generate computer choice
+
+// generate number values for RPS, then randomly choose one of the numbers, and assign the resulting choice to a variable
+
+function generateComputerChoice (){
+    var computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0){
+        return "R";
+    }
+    else if ( computerChoice == 1){
+        return "S";
+    }
+   else {
+        return  "P";
+}}
 
 
 
